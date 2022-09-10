e60e55165d092f0fa230c256547c4ffcc58892a1
Douglas Moura <douglas.ademoura@gmail.com>
2022-09-10 09:10:17 -0300

    feat(Posts): scaffold forms

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -9,11 +9,19 @@
 
 import { Router, Route, Set } from '@redwoodjs/router'
 
+import PostsLayout from 'src/layouts/PostsLayout'
+
 import BlogLayout from 'src/layouts/BlogLayout'
 
 const Routes = () => {
   return (
     <Router>
+      <Set wrap={PostsLayout}>
+        <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
+        <Route path="/posts/{id}/edit" page={PostEditPostPage} name="editPost" />
+        <Route path="/posts/{id}" page={PostPostPage} name="post" />
+        <Route path="/posts" page={PostPostsPage} name="posts" />
+      </Set>
       <Set wrap={BlogLayout}>
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
