e92cbc824e40eeca3a8e55f6f54eafedfa235c73
Douglas Moura <douglas.ademoura@gmail.com>
2022-09-10 15:16:51 -0300

    feat: add page for single posts

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -9,9 +9,8 @@
 
 import { Router, Route, Set } from '@redwoodjs/router'
 
-import PostsLayout from 'src/layouts/PostsLayout'
-
 import BlogLayout from 'src/layouts/BlogLayout'
+import PostsLayout from 'src/layouts/PostsLayout'
 
 const Routes = () => {
   return (
@@ -23,6 +22,7 @@ const Routes = () => {
         <Route path="/posts" page={PostPostsPage} name="posts" />
       </Set>
       <Set wrap={BlogLayout}>
+        <Route path="/article/{id}" page={ArticlePage} name="article" />
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
       </Set>
